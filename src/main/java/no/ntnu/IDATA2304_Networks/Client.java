db74524d189c1db0b4facd32a952dfa96f5218ba
Harald <haraldwf@stud.ntnu.no>
2022-12-03 14:21:06 +0100

    connected database
    connected database

--- src/main/java/no/ntnu/IDATA2304_Networks/Client.java
+++ src/main/java/no/ntnu/IDATA2304_Networks/Client.java
@@ -2,10 +2,21 @@ package no.ntnu.IDATA2304_Networks;
 
 import java.io.*;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.DecimalFormat;
 
 public class Client {
-    private String tableName = "tableName";
+
+  private static final String DATABASE_URL = "mysql690.loopia.se";
+  private static final String USERNAME = "group10@h328964";
+  private static final String PASSWORD = "evensivert123";
+
+  private String tableName = "tableName";
+
     private String tableAttributes = "attribute1,attribute2,attribute3"; //Not yet implemented
     public static void main(String[] args) throws IOException {
         Client client = new Client();
@@ -31,11 +42,20 @@ public class Client {
         }
     }
 
-    private void sendToDatabase(BufferedReader bf) throws IOException {
+    private void sendToDatabase(BufferedReader bf) throws IOException, SQLException {
         String sqlQuery = "INSERT INTO " + tableName + "(" + tableAttributes +
                 ") VALUES(" + bf.readLine() + ",'" + bf.readLine() + "','" + bf.readLine() + "');";
 
         System.out.println(sqlQuery);
         //Skal sendes til databasen ikke terminal vindu
+
+      Connection con = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+      System.out.println("Connection Established successfully");
+      Statement st = con.createStatement();
+      ResultSet rs = st.executeQuery(sqlQuery);
+      rs.next();
+      st.close();
+      con.close();
+      System.out.println("Connection closed.");
     }
 }
